94baf166abf19dbd727a144f55b9069fc226ae31
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-10-22 18:35:23 +0300

    add adaptive styles for mobile

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -1,5 +1,5 @@
 (() => {
-  const $dashboard = $('#display');
+  const $dashboard = $('#dashboard');
   const $lining = $('#menu-lining');
 
   const $toggler = $('#menu-toggler');
@@ -19,6 +19,8 @@
       toggle(t);
     });
 
+    $dashboard.addClass('overflow-hidden');
+
     return;
   });
 
@@ -26,6 +28,8 @@
     targets.forEach(t => {
       toggle(t);
     });
+
+    $dashboard.removeClass('overflow-hidden');
 
     return;
   });
